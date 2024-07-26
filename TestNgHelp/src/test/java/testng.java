76e7f14d12a9e1a9493fef5650dcb1f7f472ba32
Salil-Ghosh <70339727+Salil-Ghosh@users.noreply.github.com>
2024-07-26 20:20:36 +0530

    Update testng.java

--- TestNgHelp/src/test/java/testng.java
+++ TestNgHelp/src/test/java/testng.java
@@ -13,9 +13,6 @@ public class testng {
 		@BeforeSuite
 		public void runOnce() {
 			System.out.println("This is the @BeforeSuite method");
-			System.out.println("Wow Baby");
-			System.out.println("sexyBaby");
-			System.out.println("lovelyBaby");
 		}
 
 		@BeforeClass
